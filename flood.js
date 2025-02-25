07f7e6f162f1fceed3acb7884021c4e279b7619f
Isael Anjos <isael.r.anjos@gmail.com>
2025-02-25 18:47:24 -0400

    Update flood.js

--- flood.js
+++ flood.js
@@ -2,10 +2,20 @@ function exec() {
     const text = Array(100).fill().map(() => "ابتثجحخدذرزسشصضطظعغفقكلمنهوي"[Math.random() * 28 | 0]).join('');
     const t = document.querySelector("#main div[contenteditable='true']");
     if (!t) throw "Conversation not found";
-    t.focus(); document.execCommand('insertText', false, text);
+    t.focus();
+    document.execCommand('insertText', false, text);
     t.dispatchEvent(new Event('change', { bubbles: true }));
-    setTimeout(() => document.querySelector("#main [data-testid='send'], #main [data-icon='send']").click(), 100);
+    setTimeout(() => {
+        const sendButton = document.querySelector("#main [data-testid='send'], #main [data-icon='send']");
+        if (sendButton) sendButton.click();
+    }, 200);
 }
-
 let i = 0;
-const interval = setInterval(() => (++i <= 1e4 ? (exec(), i % 10 === 0 && console.log(`${i}/10000 messages sent`)) : clearInterval(interval)), 200);
+const interval = setInterval(() => {
+    if (++i <= 1e4) {
+        exec();
+        if (i % 10 === 0) console.log(`${i}/10000 messages sent`);
+    } else {
+        clearInterval(interval);
+    }
+}, 200);
